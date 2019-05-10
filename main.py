2fd6da470e55e7403308ffca8040e05fe1e3ec5d
SusmithHCK <tesla8.1997@gmail.com>
2019-05-10 16:37:44 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,372 @@
+#!/usr/bin/env python
+
+import base64
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+from sklearn import preprocessing
+
+
+# Encode text values to dummy variables(i.e. [1,0,0],[0,1,0],[0,0,1] for red,green,blue)
+def encode_text_dummy(df, name):
+    dummies = pd.get_dummies(df[name])
+    for x in dummies.columns:
+        dummy_name = f"{name}-{x}"
+        df[dummy_name] = dummies[x]
+    df.drop(name, axis=1, inplace=True)
+
+
+# Encode text values to a single dummy variable.  The new columns (which do not replace the old) will have a 1
+# at every location where the original column (name) matches each of the target_values.  One column is added for
+# each target value.
+def encode_text_single_dummy(df, name, target_values):
+    for tv in target_values:
+        l = list(df[name].astype(str))
+        l = [1 if str(x) == str(tv) else 0 for x in l]
+        name2 = f"{name}-{tv}"
+        df[name2] = l
+
+
+# Encode text values to indexes(i.e. [1],[2],[3] for red,green,blue).
+def encode_text_index(df, name):
+    le = preprocessing.LabelEncoder()
+    df[name] = le.fit_transform(df[name])
+    return le.classes_
+
+
+# Encode a numeric column as zscores
+def encode_numeric_zscore(df, name, mean=None, sd=None):
+    if mean is None:
+        mean = df[name].mean()
+
+    if sd is None:
+        sd = df[name].std()
+
+    df[name] = (df[name] - mean) / sd
+
+
+# Convert all missing values in the specified column to the median
+def missing_median(df, name):
+    med = df[name].median()
+    df[name] = df[name].fillna(med)
+
+
+# Convert all missing values in the specified column to the default
+def missing_default(df, name, default_value):
+    df[name] = df[name].fillna(default_value)
+
+
+# Convert a Pandas dataframe to the x,y inputs that TensorFlow needs
+def to_xy(df, target):
+    result = []
+    for x in df.columns:
+        if x != target:
+            result.append(x)
+    # find out the type of the target column.  Is it really this hard? :(
+    target_type = df[target].dtypes
+    target_type = target_type[0] if hasattr(
+        target_type, '__iter__') else target_type
+    # Encode to int for classification, float otherwise. TensorFlow likes 32 bits.
+    if target_type in (np.int64, np.int32):
+        # Classification
+        dummies = pd.get_dummies(df[target])
+        return df[result].values.astype(np.float32), dummies.values.astype(np.float32)
+    # Regression
+    return df[result].values.astype(np.float32), df[[target]].values.astype(np.float32)
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return f"{h}:{m:>02}:{s:>05.2f}"
+
+
+# Regression chart.
+def chart_regression(pred, y, sort=True):
+    t = pd.DataFrame({'pred': pred, 'y': y.flatten()})
+    if sort:
+        t.sort_values(by=['y'], inplace=True)
+    plt.plot(t['y'].tolist(), label='expected')
+    plt.plot(t['pred'].tolist(), label='prediction')
+    plt.ylabel('output')
+    plt.legend()
+    plt.show()
+
+# Remove all rows where the specified column is +/- sd standard deviations
+def remove_outliers(df, name, sd):
+    drop_rows = df.index[(np.abs(df[name] - df[name].mean())
+                          >= (sd * df[name].std()))]
+    df.drop(drop_rows, axis=0, inplace=True)
+
+
+# Encode a column to a range between normalized_low and normalized_high.
+def encode_numeric_range(df, name, normalized_low=-1, normalized_high=1,
+                         data_low=None, data_high=None):
+    if data_low is None:
+        data_low = min(df[name])
+        data_high = max(df[name])
+
+    df[name] = ((df[name] - data_low) / (data_high - data_low))         * (normalized_high - normalized_low) + normalized_low
+
+
+# This function submits an assignment.  You can submit an assignment as much as you like, only the final
+# submission counts.  The paramaters are as follows:
+# data - Pandas dataframe output.
+# key - Your student key that was emailed to you.
+# no - The assignment class number, should be 1 through 1.
+# source_file - The full path to your Python or IPYNB file.  This must have "_class1" as part of its name.
+# .             The number must match your assignment number.  For example "_class2" for class assignment #2.
+def submit(data,key,no,source_file=None):
+    if source_file is None and '__file__' not in globals(): raise Exception('Must specify a filename when a Jupyter notebook.')
+    if source_file is None: source_file = __file__
+    suffix = '_class{}'.format(no)
+    if suffix not in source_file: raise Exception('{} must be part of the filename.'.format(suffix))
+    with open(source_file, "rb") as image_file:
+        encoded_python = base64.b64encode(image_file.read()).decode('ascii')
+    ext = os.path.splitext(source_file)[-1].lower()
+    if ext not in ['.ipynb','.py']: raise Exception("Source file is {} must be .py or .ipynb".format(ext))
+    r = requests.post("https://api.heatonresearch.com/assignment-submit",
+        headers={'x-api-key':key}, json={'csv':base64.b64encode(data.to_csv(index=False).encode('ascii')).decode("ascii"),
+        'assignment': no, 'ext':ext, 'py':encoded_python})
+    if r.status_code == 200:
+        print("Success: {}".format(r.text))
+    else: print("Failure: {}".format(r.text))
+
+
+# # The KDD-99 Dataset
+#
+# The [KDD-99 dataset](http://kdd.ics.uci.edu/databases/kddcup99/kddcup99.html) is very famous in the security field and almost a "hello world" of intrusion detection systems in machine learning.
+#
+# # Read in Raw KDD-99 Dataset
+
+# In[2]:
+
+
+from keras.utils.data_utils import get_file
+
+try:
+    path = get_file('kddcup.data_10_percent.gz', origin='http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data_10_percent.gz')
+except:
+    print('Error downloading')
+    raise
+
+print(path)
+
+# This file is a CSV, just no CSV extension or headers
+# Download from: http://kdd.ics.uci.edu/databases/kddcup99/kddcup99.html
+df = pd.read_csv(path, header=None)
+
+print("Read {} rows.".format(len(df)))
+# df = df.sample(frac=0.1, replace=False) # Uncomment this line to sample only 10% of the dataset
+df.dropna(inplace=True,axis=1) # For now, just drop NA's (rows with missing values)
+
+# The CSV file has no column heads, so add them
+df.columns = [
+    'duration',
+    'protocol_type',
+    'service',
+    'flag',
+    'src_bytes',
+    'dst_bytes',
+    'land',
+    'wrong_fragment',
+    'urgent',
+    'hot',
+    'num_failed_logins',
+    'logged_in',
+    'num_compromised',
+    'root_shell',
+    'su_attempted',
+    'num_root',
+    'num_file_creations',
+    'num_shells',
+    'num_access_files',
+    'num_outbound_cmds',
+    'is_host_login',
+    'is_guest_login',
+    'count',
+    'srv_count',
+    'serror_rate',
+    'srv_serror_rate',
+    'rerror_rate',
+    'srv_rerror_rate',
+    'same_srv_rate',
+    'diff_srv_rate',
+    'srv_diff_host_rate',
+    'dst_host_count',
+    'dst_host_srv_count',
+    'dst_host_same_srv_rate',
+    'dst_host_diff_srv_rate',
+    'dst_host_same_src_port_rate',
+    'dst_host_srv_diff_host_rate',
+    'dst_host_serror_rate',
+    'dst_host_srv_serror_rate',
+    'dst_host_rerror_rate',
+    'dst_host_srv_rerror_rate',
+    'outcome'
+]
+
+# display 5 rows
+df[0:5]
+
+
+# # Analyzing a Dataset
+#
+# The following script can be used to give a high-level overview of how a dataset appears.
+
+# In[7]:
+
+
+ENCODING = 'utf-8'
+
+def expand_categories(values):
+    result = []
+    s = values.value_counts()
+    t = float(len(values))
+    for v in s.index:
+        result.append("{}:{}%".format(v,round(100*(s[v]/t),2)))
+    return "[{}]".format(",".join(result))
+
+def analyze(filename):
+    print()
+    print("Analyzing: {}".format(filename))
+    df = pd.read_csv(filename,encoding=ENCODING)
+    cols = df.columns.values
+    total = float(len(df))
+
+    print("{} rows".format(int(total)))
+    for col in cols:
+        uniques = df[col].unique()
+        unique_count = len(uniques)
+        if unique_count>100:
+            print("** {}:{} ({}%)".format(col,unique_count,int(((unique_count)/total)*100)))
+        else:
+            print("** {}:{}".format(col,expand_categories(df[col])))
+            expand_categories(df[col])
+
+
+# In[8]:
+
+
+# Analyze KDD-99
+
+import tensorflow.contrib.learn as skflow
+import pandas as pd
+import os
+import numpy as np
+from sklearn import metrics
+from scipy.stats import zscore
+
+
+# # Encode the feature vector
+# Encode every row in the database.  This is not instant!
+
+# In[9]:
+
+
+# Now encode the feature vector
+
+encode_numeric_zscore(df, 'duration')
+encode_text_dummy(df, 'protocol_type')
+encode_text_dummy(df, 'service')
+encode_text_dummy(df, 'flag')
+encode_numeric_zscore(df, 'src_bytes')
+encode_numeric_zscore(df, 'dst_bytes')
+encode_text_dummy(df, 'land')
+encode_numeric_zscore(df, 'wrong_fragment')
+encode_numeric_zscore(df, 'urgent')
+encode_numeric_zscore(df, 'hot')
+encode_numeric_zscore(df, 'num_failed_logins')
+encode_text_dummy(df, 'logged_in')
+encode_numeric_zscore(df, 'num_compromised')
+encode_numeric_zscore(df, 'root_shell')
+encode_numeric_zscore(df, 'su_attempted')
+encode_numeric_zscore(df, 'num_root')
+encode_numeric_zscore(df, 'num_file_creations')
+encode_numeric_zscore(df, 'num_shells')
+encode_numeric_zscore(df, 'num_access_files')
+encode_numeric_zscore(df, 'num_outbound_cmds')
+encode_text_dummy(df, 'is_host_login')
+encode_text_dummy(df, 'is_guest_login')
+encode_numeric_zscore(df, 'count')
+encode_numeric_zscore(df, 'srv_count')
+encode_numeric_zscore(df, 'serror_rate')
+encode_numeric_zscore(df, 'srv_serror_rate')
+encode_numeric_zscore(df, 'rerror_rate')
+encode_numeric_zscore(df, 'srv_rerror_rate')
+encode_numeric_zscore(df, 'same_srv_rate')
+encode_numeric_zscore(df, 'diff_srv_rate')
+encode_numeric_zscore(df, 'srv_diff_host_rate')
+encode_numeric_zscore(df, 'dst_host_count')
+encode_numeric_zscore(df, 'dst_host_srv_count')
+encode_numeric_zscore(df, 'dst_host_same_srv_rate')
+encode_numeric_zscore(df, 'dst_host_diff_srv_rate')
+encode_numeric_zscore(df, 'dst_host_same_src_port_rate')
+encode_numeric_zscore(df, 'dst_host_srv_diff_host_rate')
+encode_numeric_zscore(df, 'dst_host_serror_rate')
+encode_numeric_zscore(df, 'dst_host_srv_serror_rate')
+encode_numeric_zscore(df, 'dst_host_rerror_rate')
+encode_numeric_zscore(df, 'dst_host_srv_rerror_rate')
+outcomes = encode_text_index(df, 'outcome')
+num_classes = len(outcomes)
+
+# display 5 rows
+
+df.dropna(inplace=True,axis=1)
+df[0:5]
+# This is the numeric feature vector, as it goes to the neural net
+
+
+# # Train the Neural Network
+
+# In[6]:
+
+
+import pandas as pd
+import io
+import requests
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.callbacks import EarlyStopping
+
+# Break into X (predictors) & y (prediction)
+x, y = to_xy(df,'outcome')
+
+# Create a test/train split.  25% test
+# Split into train/test
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.25, random_state=42)
+
+# Create neural net
+model = Sequential()
+model.add(Dense(10, input_dim=x.shape[1], kernel_initializer='normal', activation='relu'))
+model.add(Dense(50, input_dim=x.shape[1], kernel_initializer='normal', activation='relu'))
+model.add(Dense(10, input_dim=x.shape[1], kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal'))
+model.add(Dense(y.shape[1],activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=1, mode='auto')
+model.fit(x_train,y_train,validation_data=(x_test,y_test),callbacks=[monitor],verbose=2,epochs=1000)
+
+model.save("trained_model.hdf5")
+
+# In[7]:
+
+
+# Measure accuracy
+pred = model.predict(x_test)
+pred = np.argmax(pred,axis=1)
+y_eval = np.argmax(y_test,axis=1)
+score = metrics.accuracy_score(y_eval, pred)
+print("Validation score: {}".format(score))
+
+
+# In[ ]:
