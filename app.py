50fd82724d7751e7b360e248a9a37b686a9f55aa
SusmithHCK <tesla8.1997@gmail.com>
2019-05-13 18:25:02 +0530

    added visualization

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+#require python 3.6 or above
 from preprocessors import *
 import tensorflow.contrib.learn as skflow
 import pandas as pd
 import os
 import io
+import random
 import requests
 import numpy as np
 from sklearn import metrics
@@ -14,12 +16,7 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.callbacks import EarlyStopping
 from keras.utils.data_utils import get_file
-import matplotlib.pyplot as plt
-
-
-# # The KDD-99 Dataset
-# Read in Raw KDD-99 Dataset
-from keras.utils.data_utils import get_file
+from keras.models import load_model
 
 try:
     path = get_file('kddcup.data_10_percent.gz', origin='http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data_10_percent.gz')
@@ -27,25 +24,10 @@ except:
     print('Error downloading')
     raise
 
-print(path)
-
-# This file is a CSV, just no CSV extension or headers
-# Download from: http://kdd.ics.uci.edu/databases/kddcup99/kddcup99.html
 df = pd.read_csv(path, header=None)
-print("Read {} rows.".format(len(df)))
-# df = df.sample(frac=0.1, replace=False) # Uncomment this line to sample only 10% of the dataset
+rawcs = df
 df.dropna(inplace=True,axis=1)
-# For now, just drop NA's (rows with missing values)
-# The CSV file has no column heads, so add them
 df.columns = ['duration','protocol_type','service','flag','src_bytes','dst_bytes','land','wrong_fragment','urgent','hot','num_failed_logins','logged_in','num_compromised','root_shell','su_attempted','num_root','num_file_creations','num_shells','num_access_files','num_outbound_cmds','is_host_login','is_guest_login','count','srv_count','serror_rate','srv_serror_rate','rerror_rate','srv_rerror_rate','same_srv_rate','diff_srv_rate','srv_diff_host_rate','dst_host_count','dst_host_srv_count','dst_host_same_srv_rate','dst_host_diff_srv_rate','dst_host_same_src_port_rate','dst_host_srv_diff_host_rate','dst_host_serror_rate','dst_host_srv_serror_rate','dst_host_rerror_rate','dst_host_srv_rerror_rate','outcome']
-
-
-# display 5 rows
-print(df[0:5])
-
-# # Encode the feature vector
-# Encode every row in the database.  This is not instant!
-# Now encode the feature vector
 
 encode_numeric_zscore(df, 'duration')
 encode_text_dummy(df, 'protocol_type')
@@ -91,54 +73,20 @@ encode_numeric_zscore(df, 'dst_host_srv_rerror_rate')
 outcomes = encode_text_index(df, 'outcome')
 num_classes = len(outcomes)
 
-# display 5 rows
 df.dropna(inplace=True,axis=1)
-print(df[0:5])
-# This is the numeric feature vector, as it goes to the neural net
-# # Train the Neural Network
-# Break into X (predictors) & y (prediction)
+
 x, y = to_xy(df,'outcome')
-# Create a test/train split.  25% test
-# Split into train/test
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.25, random_state=42)
+print("loading model...")
+model = load_model("trained_model.hdf5")
 
-# Create neural net
-print("creating neural network")
-model = Sequential()
-model.add(Dense(10, input_dim=x.shape[1], kernel_initializer='normal', activation='relu'))
-model.add(Dense(50, kernel_initializer='normal', activation='relu'))
-model.add(Dense(10, kernel_initializer='normal', activation='relu'))
-model.add(Dense(23, kernel_initializer='normal', activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
-monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=1, mode='auto')
-history = model.fit(x_train,y_train,validation_data=(x_test,y_test),callbacks=[monitor],verbose=2,epochs=1000)
-model.save("trained_model.hdf5")
-print("model saved! run app.py to test.")
-
-
-# Measure accuracy
-print(history.history.keys())
-pred = model.predict(x_test)
-pred = np.argmax(pred,axis=1)
-y_eval = np.argmax(y_test,axis=1)
-score = metrics.accuracy_score(y_eval, pred)
-print("Validation score: {}".format(score))
-# Visualize
-# Plot training & validation accuracy values
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-# Plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
+# Running random Test Case, takes random 10 inputs and predict the attack.
+r=random.sample(range(0,len(x)),10)
+for i in r:
+    test_case = x[i]
+    test_case = np.reshape(test_case,(-1,120))
+    pred = model.predict(test_case)
+    pred = np.argmax(pred, axis=1)
+    c=bcolors.GREEN
+    if pred != 11:
+        c=bcolors.RED
+    print(bcolors.WHITE+bcolors.BOLD+"\nTest case {} Input data >>".format(i)+bcolors.R+c+"\n{}\n{} ".format(rawcs.iloc[[i]],test_case)+bcolors.WHITE+"\nAttack type :"+bcolors.BOLD+c+" {}\n".format(outcomes[pred])+bcolors.R)
---
+++ preprocessors.py
@@ -0,0 +1,135 @@
+
+import base64
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+from sklearn import preprocessing
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Encode text values to dummy variables(i.e. [1,0,0],[0,1,0],[0,0,1] for red,green,blue)
+def encode_text_dummy(df, name):
+    dummies = pd.get_dummies(df[name])
+    for x in dummies.columns:
+        dummy_name = f"{name}-{x}"
+        df[dummy_name] = dummies[x]
+    df.drop(name, axis=1, inplace=True)
+
+
+# Encode text values to a single dummy variable.  The new columns (which do not replace the old) will have a 1
+# at every location where the original column (name) matches each of the target_values.  One column is added for
+# each target value.
+def encode_text_single_dummy(df, name, target_values):
+    for tv in target_values:
+        l = list(df[name].astype(str))
+        l = [1 if str(x) == str(tv) else 0 for x in l]
+        name2 = f"{name}-{tv}"
+        df[name2] = l
+
+
+# Encode text values to indexes(i.e. [1],[2],[3] for red,green,blue).
+def encode_text_index(df, name):
+    le = preprocessing.LabelEncoder()
+    df[name] = le.fit_transform(df[name])
+    return le.classes_
+
+
+# Encode a numeric column as zscores
+def encode_numeric_zscore(df, name, mean=None, sd=None):
+    if mean is None:
+        mean = df[name].mean()
+
+    if sd is None:
+        sd = df[name].std()
+
+    df[name] = (df[name] - mean) / sd
+
+
+# Convert all missing values in the specified column to the median
+def missing_median(df, name):
+    med = df[name].median()
+    df[name] = df[name].fillna(med)
+
+
+# Convert all missing values in the specified column to the default
+def missing_default(df, name, default_value):
+    df[name] = df[name].fillna(default_value)
+
+
+# Convert a Pandas dataframe to the x,y inputs that TensorFlow needs
+def to_xy(df, target):
+    result = []
+    for x in df.columns:
+        if x != target:
+            result.append(x)
+    # find out the type of the target column.  Is it really this hard? :(
+    target_type = df[target].dtypes
+    target_type = target_type[0] if hasattr(
+        target_type, '__iter__') else target_type
+    # Encode to int for classification, float otherwise. TensorFlow likes 32 bits.
+    if target_type in (np.int64, np.int32):
+        # Classification
+        dummies = pd.get_dummies(df[target])
+        return df[result].values.astype(np.float32), dummies.values.astype(np.float32)
+    # Regression
+    return df[result].values.astype(np.float32), df[[target]].values.astype(np.float32)
+
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return f"{h}:{m:>02}:{s:>05.2f}"
+
+
+# Regression chart.
+def chart_regression(pred, y, sort=True):
+    t = pd.DataFrame({'pred': pred, 'y': y.flatten()})
+    if sort:
+        t.sort_values(by=['y'], inplace=True)
+    plt.plot(t['y'].tolist(), label='expected')
+    plt.plot(t['pred'].tolist(), label='prediction')
+    plt.ylabel('output')
+    plt.legend()
+    plt.show()
+
+# Remove all rows where the specified column is +/- sd standard deviations
+def remove_outliers(df, name, sd):
+    drop_rows = df.index[(np.abs(df[name] - df[name].mean())
+                          >= (sd * df[name].std()))]
+    df.drop(drop_rows, axis=0, inplace=True)
+
+
+# Encode a column to a range between normalized_low and normalized_high.
+def encode_numeric_range(df, name, normalized_low=-1, normalized_high=1,
+                         data_low=None, data_high=None):
+    if data_low is None:
+        data_low = min(df[name])
+        data_high = max(df[name])
+
+    df[name] = ((df[name] - data_low)/(data_high - data_low))*(normalized_high - normalized_low) + normalized_low
+
+
+# This function submits an assignment.  You can submit an assignment as much as you like, only the final
+# submission counts.  The paramaters are as follows:
+# data - Pandas dataframe output.
+# key - Your student key that was emailed to you.
+# no - The assignment class number, should be 1 through 1.
+# source_file - The full path to your Python or IPYNB file.  This must have "_class1" as part of its name.
+# .             The number must match your assignment number.  For example "_class2" for class assignment #2.
+
+class bcolors:
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[31m'
+    YELLOW = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    BGRED = '\033[41m'
+    WHITE = '\033[37m'
+    R = '\033[0'
